93fcdc2b1a0dfc9eefdaf17a1abd46f204a51c30
Tomasz Leśniak <jamesshino152@gmail.com>
2020-05-24 17:14:35 +0200

    update movement

--- Platformer2DN/Assets/Scripts/Movement.cs
+++ Platformer2DN/Assets/Scripts/Movement.cs
@@ -7,7 +7,7 @@ public class Movement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("sebus");
     }
 
     // Update is called once per frame
