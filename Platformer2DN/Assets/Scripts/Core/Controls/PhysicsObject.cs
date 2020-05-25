5048964cc7440a476c600c7d9415468859d03b3d
Tomasz Leśniak <jamesshino152@gmail.com>
2020-05-25 18:48:11 +0200

    Add PhysicsObject

---
+++ Platformer2DN/Assets/Scripts/Core/Controls/PhysicsObject.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    public float gravityModifier = 1f;
+    public float minGroundNormalY = 0.65f;
+
+    protected bool isGrounded;
+    protected Vector2 groundNormal;
+    protected Vector2 velocity;
+    protected Rigidbody2D rigidBody2D = null;
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected  List<RaycastHit2D>hitBufferList = new List<RaycastHit2D>(16);
+
+    void OnEnable()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(this.gameObject.layer));
+        contactFilter.useLayerMask = true;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+
+        isGrounded = false;
+
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+
+        Vector2 move = Vector2.up * deltaPosition.y;
+        Movement(move, true);
+    }
+
+    private void Movement(Vector2 move, bool yMovement) //yMovement - vertical movement
+    {
+        float distance = move.magnitude;
+
+        if (distance > minMoveDistance)
+        {
+            int count = rigidBody2D.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+            hitBufferList.Clear();
+            for (int i = 0; i < count; i++)
+            {
+                hitBufferList.Add(hitBuffer[i]);
+            }
+
+            for (int i = 0; i < hitBufferList.Count; i++)
+            {
+                Vector2 currentNormal = hitBufferList[i].normal;
+                if (currentNormal.y > minGroundNormalY)
+                {
+                    isGrounded = true;
+                    if (yMovement)
+                    {
+                        groundNormal = currentNormal;
+                        currentNormal.x = 0;
+                    }
+                }
+
+                float projection = Vector2.Dot(velocity, currentNormal);
+                if (projection < 0)
+                {
+                    velocity = velocity - projection * currentNormal;
+                }
+
+                float modifiedDistance = hitBufferList[i].distance - shellRadius;
+                distance = modifiedDistance < distance ? modifiedDistance : distance;
+            }
+
+        }
+
+        rigidBody2D.position = rigidBody2D.position + move.normalized * distance;
+    }
+}
