b1f23058c299b6a8947d4b47eeb42dc652aacd19
Tomasz Leśniak <jamesshino152@gmail.com>
2020-05-27 14:18:49 +0200

    Add comments to Fighter script

--- Platformer2DN/Assets/Scripts/Core/Combat/Fighter.cs
+++ Platformer2DN/Assets/Scripts/Core/Combat/Fighter.cs
@@ -11,7 +11,7 @@ namespace platformer.combat
         public WeaponConfig weaponConfig = null;
         public Transform handTransform = null;
 
-        private Weapon currentWeapon;
+        private Weapon currentWeapon; // Current weapon prefab
         private WeaponConfig currentWeaponConfig;
 
         private void Awake()
@@ -20,16 +20,26 @@ namespace platformer.combat
             currentWeapon = SetupDefaultWeapon(currentWeaponConfig);
         }
 
+        /// <summary>
+        /// Initialize weapon from defaultWeaponConfig attached to a player
+        /// </summary>
+        /// <param name="defaultWeaponConfig"></param>
+        /// <returns>Weapon prefab set scriptableObject</returns>
         private Weapon SetupDefaultWeapon(WeaponConfig defaultWeaponConfig)
         {
             return defaultWeaponConfig.Spawn(handTransform, animator);
         }
 
+        /// <summary>
+        /// Instatiating a bullet from a 
+        /// </summary>
+        /// <param name="worldPoint">Coordinates of mouse position in the world space</param>
         public void Shoot(Vector3 worldPoint)
         {
             var vectorPostion = new Vector2(worldPoint.x, worldPoint.y);
             if(currentWeaponConfig.HasProjectile())
             {
+                //TODO: Change handtransform + 2 into barrel transform of weapon
                 Vector3 weaponBarrelTransform = new Vector3(handTransform.position.x + 2, handTransform.position.y, 0);
                 currentWeaponConfig.LaunchBullet(weaponBarrelTransform, vectorPostion);
 
