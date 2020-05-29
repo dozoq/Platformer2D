6f9d3c9c71476090bb80c9e9c8e8a50e8ca6af56
Tomasz Leśniak <jamesshino152@gmail.com>
2020-05-29 13:18:27 +0200

    Add persistent object spawner

---
+++ Platformer2DN/Assets/Scripts/Core/Core/PersistentObjectSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject persistentObjectToSpawn = null;
+
+    private static bool hasSpawned = false;
+
+    private void Awake()
+    {
+        if (hasSpawned) return;
+        if (persistentObjectToSpawn == null)
+        {
+            Debug.LogError("No persistent object to spawn!");
+            return;
+        }
+
+        SpawnPersistentObject();
+
+        hasSpawned = true;
+    }
+
+    private void SpawnPersistentObject()
+    {
+        GameObject persistentObject = Instantiate(persistentObjectToSpawn);
+        DontDestroyOnLoad(persistentObject);
+    }
+}
