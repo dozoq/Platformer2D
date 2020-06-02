33b5b2892faca78cc8fab54e2a3d544b9689b485
Tomasz Leśniak <jamesshino152@gmail.com>
2020-06-02 14:42:09 +0200

    Add SerializableVector3

--- Platformer2DN/Assets/Scripts/Core/Saving/SerializableVector3.cs
+++ Platformer2DN/Assets/Scripts/Core/Saving/SerializableVector3.cs
@@ -2,17 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SerializableVector3 : MonoBehaviour
+namespace platformer.saving
 {
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// Vector3 used in saving as default vector3 is not serializable
+    /// </summary>
+    
+    [System.Serializable]
+    public class SerializableVector3
     {
-        
+        private float x;
+        private float y;
+        private float z;
+
+        // Constructor
+        public SerializableVector3(Vector3 vector)
+        {
+            this.x = vector.x;
+            this.y = vector.y;
+            this.z = vector.z;
+        }
+
+        // Return new vector3 with currently saved floats in this instance
+        public Vector3 DeserializeToVector3()
+        {
+            return new Vector3(x, y, z);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
