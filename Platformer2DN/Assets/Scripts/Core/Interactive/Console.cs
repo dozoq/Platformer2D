eaea007dc3f244cb37a79e745e0ee92b7f467227
Tomasz Leśniak <jamesshino152@gmail.com>
2020-06-08 13:40:49 +0200

    Console and elevator scripts

---
+++ Platformer2DN/Assets/Scripts/Core/Interactive/Console.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace platformer.interactive
+{
+    public class Console : MonoBehaviour
+    {
+        [Tooltip("GameObject(with IInteractionHandler) that this console activates")]
+        [SerializeField] private GameObject triggerObject = null;
+
+        IInteractionHandler interactiveObject = null;
+
+        private void Start()
+        {
+            interactiveObject = triggerObject.GetComponent<IInteractionHandler>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if(interactiveObject != null)
+                {
+                    interactiveObject.HandleInteraction(this.gameObject);
+                }
+            }
+        }
+    }
+
+}
---
+++ Platformer2DN/Assets/Scripts/Core/Interactive/Elevator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace platformer.interactive
+{
+    public class Elevator : MonoBehaviour, IInteractionHandler
+    {
+        public void HandleInteraction(GameObject who)
+        {
+            Debug.LogWarning("Elevator activated by " + who);
+        }
+    }
+
+}
