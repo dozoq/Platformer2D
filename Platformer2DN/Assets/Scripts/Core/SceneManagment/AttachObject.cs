b41aedae871b314d26b4f923d69f59fc3516cc48
Tomasz Leśniak <jamesshino152@gmail.com>
2020-06-11 17:17:16 +0200

    AttachObject documentation

--- Platformer2DN/Assets/Scripts/Core/SceneManagment/AttachObject.cs
+++ Platformer2DN/Assets/Scripts/Core/SceneManagment/AttachObject.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 namespace platformer.scenemanagment
 {
+    /// <summary>
+    /// Use this script whenever you want to attach "dynamic" object to 
+    /// another object that is moving horizontally
+    /// </summary>
     public class AttachObject : MonoBehaviour
     {
         [SerializeField] private bool attachPlayerOnly = true;
@@ -12,14 +16,19 @@ namespace platformer.scenemanagment
         private Vector3 previousTransform;
         private bool isPreviousTransformSet = false;
 
+        /// <summary>
+        /// Activates whenever object with collider/rigidbody is on object of this component
+        /// </summary>
         private void OnCollisionStay2D(Collision2D collision)
         {
+            // If can move only player, and the the collider is not a player, return
             if(attachPlayerOnly && !collision.gameObject.CompareTag("Player"))
             {
                 Debug.LogWarning("Object not a player");
                 return;
             }
 
+            // Check if transform changed horizontally
             if (isPreviousTransformSet && Mathf.Abs(previousTransform.x - this.transform.position.x) > 0)
             {
                 float difference = previousTransform.x - this.transform.position.x;
@@ -31,8 +40,18 @@ namespace platformer.scenemanagment
             isPreviousTransformSet = true;
         }
 
+        /// <summary>
+        /// Left the collider
+        /// </summary>
         private void OnCollisionExit2D(Collision2D collision)
         {
+            // If can move only player, and the the collider is not a player, return
+            if (attachPlayerOnly && !collision.gameObject.CompareTag("Player"))
+            {
+                Debug.LogWarning("Object not a player");
+                return;
+            }
+
             isPreviousTransformSet = false;
             print("Leaved elevator");
         }
