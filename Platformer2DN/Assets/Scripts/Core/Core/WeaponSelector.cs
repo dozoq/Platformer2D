2fe20eaa0a30467951faf5d9c16af1906082f944
Tomasz Leśniak <jamesshino152@gmail.com>
2020-06-15 14:57:04 +0200

    Raw UI of weapon selector

---
+++ Platformer2DN/Assets/Scripts/Core/Core/WeaponSelector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace platformer.core
+{
+    public class WeaponSelector : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
